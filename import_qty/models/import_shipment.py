88b24891ddac4767cb2ffb0648c6bcc3cdb2b375
TugayHatil <tugay.hatill@gmail.com>
2026-06-19 18:13:21 +0300

    Add free_qty column to show unreserved stock quantity

--- import_qty/models/import_shipment.py
+++ import_qty/models/import_shipment.py
@@ -13,6 +13,15 @@ class ImportShipment(models.Model):
         help="Current stock quantity from product inventory"
     )
 
+    free_qty = fields.Float(
+        string='Free Qty',
+        compute='_compute_free_qty',
+        store=True,
+        readonly=True,
+        group_operator='avg',
+        help="Free quantity (available - reserved) from product inventory"
+    )
+
     @api.depends('product_id')
     def _compute_stock_qty(self):
         for record in self:
@@ -20,3 +29,11 @@ class ImportShipment(models.Model):
                 record.stock_qty = record.product_id.qty_available
             else:
                 record.stock_qty = 0.0
+
+    @api.depends('product_id')
+    def _compute_free_qty(self):
+        for record in self:
+            if record.product_id:
+                record.free_qty = record.product_id.free_qty
+            else:
+                record.free_qty = 0.0
